8e28d3c3b12d49b4ac2b437c372145e0e1525f9e
François Rullière <kapochamo@gmail.com>
2023-08-12 11:28:41 +0200

    add node

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,16 +1,17 @@
 import * as yup from 'yup';
 
-const schema = yup.object({
-  from: yup.string().url().required(),
-  to: yup.string().url().required()
-}).required()
+const validate = (data) => {
+  const from = new URL(data.from).toString()
+  const to = new URL(data.to).toString()
+  return { from, to }
+}
 export async function onRequestPut({ request, env }) {
   try {
     const body = await request.json()
     const { from, to } = await schema.validate(body)
     const key = from.replace(/^https?:\/\//, '')
-    
-    console.log({key, to})
+
+    console.log({ key, to })
     await env.REDIRECTED_KV.put(key, to)
     return new Response('All good!', { status: 200 })
   } catch (e) {
