4d260656180a9a40e0cd1e84ae04b354fff43d14
François Rullière <kapochamo@gmail.com>
2023-08-12 10:48:42 +0200

    string

--- functions/health.js
+++ functions/health.js
@@ -1,7 +1,7 @@
 export function onRequest(context,) {
   return new Response(JSON.stringify(context), {
     headers: {
-      body: context.request.body.toString()
+      body: JSON.stringify(context.request.body)
     }
   })
 }
