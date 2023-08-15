26abe5d3497015f5aa3ea3b924d38e1e920311f4
François Rullière <kapochamo@gmail.com>
2023-08-15 19:40:20 +0200

    fix path

--- src/middlewares/static.ts
+++ src/middlewares/static.ts
@@ -1,13 +1,16 @@
 import { Middleware } from "../lib/types.js";
 import fs from 'fs';
 import path from "path"
+//@ts-ignore ???
+import { fileURLToPath } from "url"
 
-const rootDir = path.dirname(import.meta.url)
+const __filename = fileURLToPath(import.meta.url)
+const baseDir = path.join(path.dirname(__filename), '../..')
 
 // HTML
 export const rootWebsite: Middleware = async (ctx, next) => {
   if (ctx.path !== '/') return next()
-  const pathName = path.join(rootDir, 'dist/index.html')
+  const pathName = path.join(baseDir, 'dist/index.html')
   ctx.type = 'html';
   ctx.body = fs.createReadStream(pathName);
 }
@@ -16,8 +19,10 @@ export const rootWebsite: Middleware = async (ctx, next) => {
 export const wellKnownForCerts: Middleware = async (ctx, next) => {
   const file = /^\/\.well-known\/(.+)$/.exec(ctx.path)?.[1]
   if (file == null) return next()
+  if (file.includes('..')) return next()
+  if (file.endsWith('/')) return next()
 
-  const wellKnownPath = path.join(__dirname, 'src/.well-known/' + file)
+  const wellKnownPath = path.join(baseDir, 'src/.well-known/' + file)
   const exists = fs.existsSync(wellKnownPath)
   console.log('Looking for ', { file, wellKnownPath, exists })
   if (exists) {
