5498ee3e6f593da08d6b80dd224b4ac1752af871
François Rullière <kapochamo@gmail.com>
2023-08-19 12:18:55 +0200

    fix links

--- src/middlewares/log-request.ts
+++ src/middlewares/log-request.ts
@@ -4,15 +4,17 @@ import { UAParser } from 'ua-parser-js'
 export const logRequest: Middleware = async (ctx, next) => {
   await next()
   if (ctx.method === 'OPTIONS') return
-  const isStaticAsset = /\.\w{2,}$/.test(ctx.url)
+  const isStaticAsset = /\.\w{2,}$/.test(ctx.originalUrl)
+  console.log({ isStaticAsset }, ctx.hostname, ctx.method)
   if (ctx.hostname === 'redirected.app' || ctx.hostname === 'localhost') {
     if (ctx.method !== 'GET' || isStaticAsset) return
   }
+  console.log('saving ...')
 
   const ua = new UAParser(ctx.req.headers['user-agent'])
   void db.run(
     `INSERT INTO travels ( domain, pathname, createdAt, ip, method, referrer, userAgent, userAgentBrowser, userAgentOs, userAgentDevice, status, redirectedTo ) 
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`,
     [
       ctx.hostname,
       ctx.url,
@@ -21,9 +23,9 @@ export const logRequest: Middleware = async (ctx, next) => {
       ctx.method,
       ctx.req.headers.referer,
       ua.getUA(),
-      ua.getBrowser().name + '/' + ua.getBrowser().version,
-      ua.getOS().name + '/' + ua.getOS().version,
-      ua.getDevice().vendor + '/' + ua.getDevice().model + '/' + ua.getDevice().type,
+      [ua.getBrowser().name, ua.getBrowser().version].map(v => v || '?').join('/'),
+      [ua.getOS().name, ua.getOS().version].map(v => v || '?').join('/'),
+      [ua.getDevice().vendor, ua.getDevice().model, ua.getDevice().type].map(v => v || '?').join('/'),
       ctx.status,
       ctx.response.headers['location']
     ]
