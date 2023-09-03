d64bf1887f37bcde7093defdb5c5caad14c1bd71
François Rullière <kapochamo@gmail.com>
2023-09-03 18:04:46 +0200

    remove unused

--- src/api2/index.ts
+++ src/api2/index.ts
@@ -1,8 +1,10 @@
+import fs from 'fs/promises'
 import Router from '@koa/router'
 import { Redirect, db } from "../lib/database.js"
 import { hasCertificate } from "../api/dns.js"
 import { render } from "../middlewares/render.js"
 import { createCertificate, getCname } from './dns.js'
+import { projectRoot } from '../lib/helpers.js'
 
 const isProd = process.env.NODE_ENV === 'production'
 const DISABLE_OUR_REDIRECT = false
@@ -70,6 +72,37 @@ router.post('/dns', async ctx => {
     await createCertificate(domain)
     ctx.status = 200
   }
+})
+
+router.get('/stats', async (ctx, next) => {
+  const statPathFile = projectRoot() + '/../.stats-path'
+  const statPath = await fs.readFile(statPathFile).then(d => d.toString().trim()).catch(() => '/stats');
+
+  if (ctx.path !== statPath || ctx.method !== 'GET') return next();
+
+  const travels = await db.all<Array<{ domain: string, pathname: string, count: number }>>(`
+      SELECT domain, pathname, COUNT(status) as count 
+      FROM travels
+      WHERE status < 400 AND ( redirectedTo IS NULL OR redirectedTo != 'https://redirected.app' )
+      GROUP BY domain, pathname
+      ORDER by count DESC, domain;`
+  )
+  const entries = await db.all<Array<{ domain: string, pathname: string, count: number }>>(`
+      SELECT domain, ('/'||pathname) as pathname, 0 as count 
+      FROM redirects
+      WHERE deletedAt IS NULL;`
+  )
+  let travelsGrp: Record<string, Record<string, number>> = {};
+  [...travels, ...entries].forEach(res => {
+    const hasCert = hasCertificate(res.domain)
+    const domain = hasCert ? '*' + res.domain : res.domain;
+    if (travelsGrp[domain] == null) travelsGrp[domain] = {}
+    travelsGrp[domain][res.pathname] = travelsGrp[domain][res.pathname] || res.count;
+  })
+
+  travelsGrp = Object.fromEntries(Object.entries(travelsGrp).sort(([keyA], [keyB]) => keyA.localeCompare(keyB)))
+
+  ctx.body = travelsGrp
 })
 
 
